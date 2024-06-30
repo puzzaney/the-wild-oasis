0f74af7a6ca6bcf1d862dfb93a64ec6fd2ff9b89
PuzZan <mail.puzzan@gmail.com>
2024-06-30 10:51:07 +0545

    Created custom hook for updating setting functionality

---
+++ src/features/settings/useUpdateSetting.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { updateSetting as updateSettingApi } from '../../services/apiSettings';
+import toast from 'react-hot-toast';
+
+export function useUpdateSetting() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
+    mutationFn: (newSetting) => updateSettingApi(newSetting),
+    onSuccess: () => {
+      toast.success('Setting Edited Successfully');
+      queryClient.invalidateQueries({ queryKey: ['cabins'] });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  return { updateSetting, isUpdating };
+}
