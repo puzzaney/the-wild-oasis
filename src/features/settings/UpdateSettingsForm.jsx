0a8e8c79e08baf02ce1431e3549295197db9bcb0
PuzZan <mail.puzzan@gmail.com>
2024-06-30 09:58:05 +0545

    Added default values to the input elements in the form

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -4,23 +4,37 @@ import Input from '../../ui/Input';
 import { useSettings } from './useSettings';
 
 function UpdateSettingsForm() {
-  const { isLoading, settings } = useSettings();
-
-  console.log(settings);
+  const {
+    isLoading,
+    settings: {
+      minBookingLength,
+      maxBookingLength,
+      maxGuestsPerBooking,
+      breakfastPrice,
+    } = {},
+  } = useSettings();
 
   return (
     <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" />
+        <Input type="number" id="min-nights" defaultValue={minBookingLength} />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" />
+        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
       </FormRow>
       <FormRow label="Maximum guests/booking">
-        <Input type="number" id="max-guests" />
+        <Input
+          type="number"
+          id="max-guests"
+          defaultValue={maxGuestsPerBooking}
+        />
       </FormRow>
       <FormRow label="Breakfast price">
-        <Input type="number" id="breakfast-price" />
+        <Input
+          type="number"
+          id="breakfast-price"
+          defaultValue={breakfastPrice}
+        />
       </FormRow>
     </Form>
   );
