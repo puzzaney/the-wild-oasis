598fb851547af60cf0161a791e7737b839fffa8f
puzzaney <poojankarki12@gmail.com>
2024-07-04 21:02:00 +0545

    Extracted custom hook to listen to click outside modal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -9,6 +9,7 @@ import {
 import { createPortal } from 'react-dom';
 import { HiXMark } from 'react-icons/hi2';
 import styled from 'styled-components';
+import { useOutsideClick } from '../hooks/useOutsideClick';
 
 const StyledModal = styled.div`
   position: fixed;
@@ -82,20 +83,8 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
-  const ref = useRef();
 
-  useEffect(
-    function () {
-      function handleClick(e) {
-        if (ref.current && !ref.current.contains(e.target)) close();
-      }
-
-      document.addEventListener('click', handleClick, true);
-
-      return () => document.removeEventListener('click', handleClick, true);
-    },
-    [close]
-  );
+  const ref = useOutsideClick(close);
 
   if (name !== openName) return null;
 
